2405c45b8a05b3d120eb4dd97e9104f0f0ab6811
divya-vasoya <144533250+Divyavasoya@users.noreply.github.com>
2025-03-05 11:23:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+document.getElementById("btn").addEventListener("click", function() {
+            const name = document.getElementById("name").value.trim();
+            const age = document.getElementById("age").value.trim();
+
+            if (!name || !age) {
+                alert("Please enter valid details.");
+                return;
+            }
+
+            new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    if (parseInt(age) > 18) {
+                        resolve(`Welcome, ${name}. You can vote.`);
+                    } else {
+                        reject(`Oh sorry ${name}. You aren't old enough.`);
+                    }
+                }, 4000);
+            })
+            .then(message => alert(message))
+            .catch(error => alert(error));
+        });
